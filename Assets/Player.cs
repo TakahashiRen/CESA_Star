0799a60c0b3b685e8d54821010bc2f55404a8462
TsujiiEijirou <tsujii.eijirou1689@gmail.com>
2019-04-08 05:18:01 +0900

    プレイヤーとアンカーの移動

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector2 Pos;
+    Vector2 Vec;
+    float Sca;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pos = Vector2.zero;
+        Vec = Vector2.zero;
+        Sca = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(0.0f,0.2f,0.0f);
+    }
+}
