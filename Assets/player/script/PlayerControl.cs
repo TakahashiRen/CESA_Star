bc40d67d98b5a2e9461350b640ec47d999b7d649
nakagawatenma <tenko1177@gmail.com>
2019-04-08 22:39:00 +0900

    プレイヤーの基本動作

--- Assets/player/script/PlayerControl.cs
+++ Assets/player/script/PlayerControl.cs
@@ -9,11 +9,14 @@ public class PlayerControl : MonoBehaviour
 {
     // 速さ
     public float m_speed;
+    public float m_anchorSpeed;
     // アンカー
     public List<GameObject> m_anchor = new List<GameObject>();
+    public List<AnchorCollision> m_anchorCollList = new List<AnchorCollision>();
     // アンカーの移動範囲(半径設定)
     public float m_len;
-    
+    public float m_returnSpeed;
+
     // アンカーの状態 
     // 0 = プレイヤーにくっついている
     // 1 = 離れている(移動中
@@ -41,6 +44,8 @@ public class PlayerControl : MonoBehaviour
             m_anchorFlag.Add(0);
             // 初期位置の保存
             m_startPosition.Add(m_anchor[i].transform.localPosition);
+            // アンカーの当たり判定情報
+            m_anchorCollList.Add(m_anchor[i].GetComponent<AnchorCollision>());
         }    
     }
 
@@ -49,11 +54,42 @@ public class PlayerControl : MonoBehaviour
     /// </summary>
     void Update()
     {
-        transform.position += Vector3.up * m_speed;
         // アンカーの更新
         AnchorUpdate();
+        // プレイヤーの移動
+        PlayerMove();
+        // アンカーのリセット
+        ResetAnchorCollision();
     }
 
+    /// <summary>
+    /// プレイヤーの移動
+    /// </summary>
+    void PlayerMove()
+    {
+        // 状態リスト
+        List<int> state = new List<int>();
+        for (int i = 0; i < m_anchorFlag.Count; i++) {
+            // 固定状態の確認
+            if (m_anchorFlag[i] == 2) {
+                // 固定先の保存
+                state.Add(i);
+            }
+        }
+
+        // 固定先がない時
+        if(state.Count == 0) {
+            transform.position += transform.up * m_speed;
+        } else  if(state.Count == 1){
+            // 固定先が一つの時
+            if (state[0] == (int)StrKey.W || state[0] == (int)StrKey.Q)
+                transform.RotateAround(m_anchor[state[0]].transform.position, transform.forward, 5 * m_speed);
+            else
+                transform.RotateAround(m_anchor[state[0]].transform.position, -transform.forward, 5 * m_speed);
+            } else {
+            // 何もしない 
+        }
+    }
 
     /// <summary>
     /// アンカーの発射
@@ -72,7 +108,10 @@ public class PlayerControl : MonoBehaviour
     void AnchorMove(int  num)
     {
         m_anchor[num].transform.position += m_anchor[num].transform.up * m_speed;
-
+        if (m_anchorCollList[num].m_collisionFlag) {
+            m_anchorFlag[num] = 2;
+            return;
+        }
         // 最大距離の計測
         if (Vector3.Distance(m_anchor[num].transform.position, transform.position) > m_len) {
             m_anchorFlag[num] = 3;
@@ -80,12 +119,25 @@ public class PlayerControl : MonoBehaviour
     }
 
     /// <summary>
-    /// アンカーの固定
+    /// アンカーの当たり判定のリセット
+    /// </summary>
+    void ResetAnchorCollision()
+    {
+        for(int i= 0; i < m_anchorFlag.Count; i++) {
+            if(m_anchorFlag[i] != 2) {
+                m_anchorCollList[i].m_collisionFlag = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// アンカーの固定状態の時
     /// </summary>
     /// <param name="num">アンカー番号</param>
     void AnchorFixed(int num)
     {
-
+        // キー判定
+        if (Input.GetKey(m_key[num])) m_anchorFlag[num] = 3;
     }
 
     /// <summary>
@@ -94,15 +146,18 @@ public class PlayerControl : MonoBehaviour
     /// <param name="num">アンカー番号</param>
     void AnchorReturn(int num)
     {        
-        m_anchor[num].transform.position += -(m_anchor[num].transform.up) * m_speed;
+        m_anchor[num].transform.position += -(m_anchor[num].transform.up) * m_returnSpeed;
 
         // 元の位置に戻ったかの確認
         if(Vector3.Distance(m_anchor[num].transform.localPosition, m_startPosition[num]) < 0.2f) {
+            // 当たり判定を初期化
+            m_anchorCollList[num].m_collisionFlag = false;
             // 少しのずれを調整
             m_anchor[num].transform.localPosition = m_startPosition[num];
             // 状態を戻すS
             m_anchorFlag[num] = 0;
         }
+       
     }
 
     /// <summary>
@@ -125,8 +180,9 @@ public class PlayerControl : MonoBehaviour
                     AnchorMove(i);
                     break;
 
-                    // 離れている(壁に引っ付き状態
+                    // アンカー固定(壁に引っ付き状態
                 case 2:
+                    AnchorFixed(i);
                     break;
 
                     // 戻っている(移動中
